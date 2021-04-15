2dec00b77060d3511b0fc375c43ca03cfe5aa9e9
Rob Bassi <rob.bassi.89@gmail.com>
2021-04-14 20:47:37 -0400

    Add updates/overwites to tests (#8)

--- test/CommitLogSpec.hs
+++ test/CommitLogSpec.hs
@@ -4,12 +4,13 @@ import BinIO (kvFold)
 import qualified CommitLog
 import Common
 import Data.Foldable (for_)
+import qualified Memtable
 import Test.Hspec (SpecWith, describe, it)
 import Test.QuickCheck (Property, arbitrary, generate, property, withMaxSuccess)
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 import Types hiding (commitLogPath)
 
-buildCommitLog :: IO ([(Key, Value)], FilePath)
+buildCommitLog :: IO ([Entry], FilePath)
 buildCommitLog = do
   commitLogPath <- mkTempFilePath "commitlog" ".bin"
   (_, commitLog) <- CommitLog.resume commitLogPath
@@ -22,7 +23,10 @@ prop_roundtrip :: Property
 prop_roundtrip = monadicIO $ do
   (input, commitLogPath) <- run buildCommitLog
   entriesOut <- run $ reverse <$> kvFold commitLogPath (:) []
+  (memtable, _) <- run $ CommitLog.resume commitLogPath
+  memtableEntries <- run $ Memtable.entries memtable
   assert $ input == entriesOut
+  assert $ latestEntries input == memtableEntries
 
 tests :: SpecWith ()
 tests = describe "CommitLog" $ do
