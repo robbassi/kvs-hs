f29438f73e63ce26ae7eb0294c970ab4a8352811
Rob Bassi <rob@mercury.com>
2021-04-14 20:42:24 -0400

    Add updates/overwites to tests

--- test/Common.hs
+++ test/Common.hs
@@ -3,15 +3,34 @@
 module Common where
 
 import Data.Function (on)
-import Data.List (nubBy)
+import Data.List (foldl', nubBy, sortOn)
+import Data.Traversable (for)
 import Data.Word (Word64)
 import Orphans
 import System.CPUTime (getCPUTime)
 import Test.QuickCheck (Gen, arbitrary, generate)
-import Types (Key, Value)
+import Test.QuickCheck.Gen (sublistOf)
+import Types
 
-entriesUniqueByKey :: Gen [(Key, Value)]
+entriesUniqueByKey :: Gen [Entry]
 entriesUniqueByKey = nubBy ((==) `on` fst) <$> arbitrary
+
+entriesWithDuplicateKeys :: Gen [Entry]
+entriesWithDuplicateKeys = do
+  entries <- arbitrary
+  dups <- sublistOf entries
+  updatedEntries <- for dups $ \(k, _) -> (k,) <$> arbitrary
+  pure $ entries ++ updatedEntries
+
+latestEntries :: [Entry] -> [Entry]
+latestEntries = reverse . foldl' merge [] . sortOn fst
+  where
+    merge :: [Entry] -> Entry -> [Entry]
+    merge (prev@(key, _) : rest) next@(key', _) =
+      if key == key'
+        then next : rest
+        else next : prev : rest
+    merge [] kv = [kv]
 
 mkTempFilePath :: String -> String -> IO FilePath
 mkTempFilePath prefix affix = do
--- test/RBTreeSpec.hs
+++ test/RBTreeSpec.hs
@@ -3,35 +3,33 @@ module RBTreeSpec where
 import Common
 import Data.Foldable (for_)
 import Data.List (sortOn)
-import Data.Maybe (fromJust)
-import Data.Traversable (for)
 import RBTree (RBTree)
 import qualified RBTree
 import Test.Hspec (SpecWith, describe, it)
-import Test.QuickCheck (Property, generate, property, withMaxSuccess)
+import Test.QuickCheck (Gen, Property, generate, property, withMaxSuccess)
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 import Types
 
-buildTree :: IO ([(Key, Value)], RBTree)
-buildTree = do
+buildTree :: Gen [Entry] -> IO ([(Key, Value)], RBTree)
+buildTree input = do
   tree <- RBTree.empty
-  input <- generate entriesUniqueByKey
-  for_ input (uncurry $ RBTree.insert tree)
-  pure (input, tree)
+  input' <- generate input
+  for_ input' (uncurry $ RBTree.insert tree)
+  pure (input', tree)
 
 prop_order :: Property
 prop_order = monadicIO $ do
-  (input, tree) <- run buildTree
+  (input, tree) <- run $ buildTree entriesUniqueByKey
   output <- run $ RBTree.toList tree
   let sortedInput = sortOn fst input
   assert $ output == sortedInput
 
 prop_association :: Property
 prop_association = monadicIO $ do
-  (input, tree) <- run buildTree
-  valuesOut <- run $ for input (fmap fromJust . RBTree.search tree . fst)
-  let valuesIn = snd <$> input
-  assert $ valuesIn == valuesOut
+  (input, tree) <- run $ buildTree entriesWithDuplicateKeys
+  entriesOut <- run $ RBTree.toList tree
+  let entriesIn = latestEntries input
+  assert $ entriesIn == entriesOut
 
 tests :: SpecWith ()
 tests = describe "RBTree" $ do
