db50b9c1aef2e545eca65e813293c172206e7429
Rob Bassi <rob@mercury.com>
2021-04-14 21:56:52 -0400

    Added test for duplicate inserts

--- test/MemtableSpec.hs
+++ test/MemtableSpec.hs
@@ -1,14 +1,14 @@
 module MemtableSpec where
 
 import Common
+import Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString as BS
 import Data.Coerce (coerce)
 import Data.Foldable (for_)
-import Data.Maybe (fromJust)
-import Data.Traversable (for)
 import Memtable (Memtable)
 import qualified Memtable
 import Test.Hspec (SpecWith, describe, it)
+import Test.Hspec.Expectations (shouldBe)
 import Test.QuickCheck (Property, generate, property, withMaxSuccess)
 import Test.QuickCheck.Monadic (assert, monadicIO, run)
 import Types
@@ -19,7 +19,7 @@ approximateBytesThreshold = 1024
 buildMemtable :: IO ([Entry], Memtable)
 buildMemtable = do
   memtable <- Memtable.empty
-  input <- generate entriesUniqueByKey
+  input <- generate entriesWithDuplicateKeys
   for_ input (uncurry $ Memtable.set memtable)
   pure (input, memtable)
 
@@ -33,20 +33,29 @@ entrySize (key, Value valueBytes) = keySize + valueSize
 prop_minByteCount :: Property
 prop_minByteCount = monadicIO $ do
   (input, memtable) <- run buildMemtable
-  let inputSize = sum $ entrySize <$> input
+  let inputSize = sum $ entrySize <$> latestEntries input
   byteCount <- run $ Memtable.approximateBytes memtable
   assert $ abs (byteCount - inputSize) <= approximateBytesThreshold
 
 prop_association :: Property
 prop_association = monadicIO $ do
   (input, memtable) <- run buildMemtable
-  valuesOut <- run $ for input (fmap fromJust . Memtable.get memtable . fst)
-  let valuesIn = snd <$> input
-  assert $ valuesIn == valuesOut
+  valuesOut <- run $ Memtable.entries memtable
+  assert $ latestEntries input == valuesOut
 
 tests :: SpecWith ()
 tests = describe "Memtable" $ do
   it "keeps track of memory usage" $ do
-    property $ withMaxSuccess 10000 prop_minByteCount
+    property $ withMaxSuccess 100000 prop_minByteCount
   it "get returns the correct values" $ do
     property $ withMaxSuccess 10000 prop_association
+  it "does not count duplicate inserts" $ do
+    let emptyEntry = (Key "A", Tombstone)
+        expectedSize = entrySize emptyEntry
+    byteCount <- liftIO $ do
+      memtable <- Memtable.empty
+      for_ [1 .. approximateBytesThreshold] $
+        const $ do
+          uncurry (Memtable.set memtable) emptyEntry
+      Memtable.approximateBytes memtable
+    byteCount `shouldBe` expectedSize
